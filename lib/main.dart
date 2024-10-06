4c970840383fe6464245200eb01d184914b7181a
treestae <treestae@gmail.com>
2024-10-06 23:46:34 +0900

    Initial commit + 한글 전용으로 글자 세기 + 붙여넣기 버튼 + 초기화 버튼 + AOS, macOS 아이콘 추가

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '서예용 글자수 세기',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  final tfController = TextEditingController();
+
+  final hangulRegex = RegExp(r'[가-힣]');
+  final emptyRegex = RegExp(r'\s+');
+
+  String inputText = '';
+
+  @override
+  void dispose() {
+    tfController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String onlyHangul = hangulRegex.allMatches(inputText).map((e) => e.group(0) ?? '').join();
+
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 8),
+                TextField(
+                  controller: tfController,
+                  onChanged: (value) => setState(() {
+                    inputText = value;
+                  }),
+                  decoration: const InputDecoration(
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelText: '글자수 세기',
+                    hintText: '글자수를 세고 싶은 글을 입력하세요.',
+                    border: OutlineInputBorder(),
+                  ),
+                  maxLines: null,
+                  minLines: 10,
+                  keyboardType: TextInputType.multiline,
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => setState(() {
+                        Clipboard.getData('text/plain').then((value) {
+                          tfController.text = value?.text ?? '';
+                          inputText = value?.text ?? '';
+                        });
+                        tfController.clear();
+                        inputText = '';
+                      }),
+                      child: const Text('붙여넣기'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => setState(() {
+                        tfController.clear();
+                        inputText = '';
+                      }),
+                      child: const Text('초기화'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text('모든 문자: ${inputText.length}'),
+                const SizedBox(height: 4),
+                Text('공백 제외: ${inputText.replaceAll(emptyRegex, '').length}', style: const TextStyle(color: Colors.green)),
+                const SizedBox(height: 4),
+                Text('한글만: ${onlyHangul.length}', style: const TextStyle(color: Colors.redAccent)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
